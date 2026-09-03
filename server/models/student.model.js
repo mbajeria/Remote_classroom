c23ab97f6c08777391fdffffa110e056d6a0d69b
Manish <manishbajeria24@gmail.com>
2026-09-03 21:38:58 +0530

    Revert "Feature/manish backend modules"

--- server/models/student.model.js
+++ server/models/student.model.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const studentSchema = new mongoose.Schema({
   fullName: {
@@ -11,40 +10,31 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    trim: true,
-    lowercase: true,
   },
   phone: {
     type: Number,
     required: true,
   },
-  password: {
-    type: String,
-    required: true,
+  password:{
+    type:String,
+    required:true
   },
   course: {
     type: String,
     required: true,
-    trim: true,
   },
   currentYear: {
     type: Number, // Example: 1, 2, 3, 4
     required: true,
-    min: 1,
-    max: 5,
   },
-  profileImage: {
-    type: String,
-    default: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+  profileImage:{
+    type:String,
+    // default:"https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
   },
   role: {
     type: String,
     enum: ["student"],
     default: "student",
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
   },
   createdAt: {
     type: Date,
@@ -52,17 +42,6 @@ const studentSchema = new mongoose.Schema({
   },
 });
 
-// Indexes for fast lookup
-studentSchema.index({ email: 1 });
-studentSchema.index({ phone: 1 });
-studentSchema.index({ course: 1, currentYear: 1 });
-
-// Method to verify password
-studentSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
 const studentModel = mongoose.model("Student", studentSchema);
 
 module.exports = studentModel;
-
